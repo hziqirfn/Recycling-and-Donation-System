aadfaff542a5d8f585f1a8ed3762953632a8a8db
KimzRawr00 <hakimmanan4@gmail.com>
2026-06-08 22:23:59 +0800

    kerjabaru

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,38 @@
 document.getElementById("content").addEventListener("click", () => {
     document.getElementById("cb").checked = false;
 })
+
+let activityOpen = false;
+let contributorOpen = false;
+
+function toggleActivity(){
+
+    const items = document.querySelectorAll(".hiddenActivity");
+    const button = document.querySelector("#activityList")
+                    .previousElementSibling
+                    .querySelector("button");
+
+    activityOpen = !activityOpen;
+
+    items.forEach(item=>{
+        item.style.display = activityOpen ? "list-item" : "none";
+    });
+
+    button.textContent = activityOpen ? "Hide" : "View All";
+}
+
+function toggleContributor(){
+
+    const items = document.querySelectorAll(".hiddenContributor");
+    const button = document.querySelector("#contributorList")
+                    .previousElementSibling
+                    .querySelector("button");
+
+    contributorOpen = !contributorOpen;
+
+    items.forEach(item=>{
+        item.style.display = contributorOpen ? "list-item" : "none";
+    });
+
+    button.textContent = contributorOpen ? "Hide" : "View All";
+}
